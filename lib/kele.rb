a9c54794b087042342e3a1fa190e2cdb19787f3a
Archita Patel <architapatelis@gmail.com>
2017-05-06 16:44:26 -0700

    add get_messages and create_message methods

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,9 +9,11 @@ class Kele
   include HTTParty
   include Roadmap
 
+  base_uri "https://www.bloc.io/api/v1"
+
   def initialize(email, password)
-    @bloc_base_api_url = "https://www.bloc.io/api/v1"
-    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {email: email, password: password})
+    #@bloc_base_api_url = "https://www.bloc.io/api/v1"
+    response = self.class.post("/sessions", body: {email: email, password: password})
 
     @auth_token = response["auth_token"]
 
@@ -19,17 +21,31 @@ class Kele
   end
 
   def get_me
-    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
+    response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
     @user_data = JSON.parse(response.body)
-
-    #puts "name: #{@user_data['name']}\n" + "email: #{@user_data['email']}\n"
   end
 
-  def get_mentor_availability(mentor_id)
-    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token})
+  def get_mentor_availability
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token})
 
     @mentor_availability = JSON.parse(response.body)
 
+  end
+
+  def get_messages(page = nil)
+    if page == nil
+      total_num_of_pages = (JSON.parse(self.class.get("/message_threads", headers: { "authorization" => @auth_token }).body)["count"])/10+1
+
+      @messages = (1..total_num_of_pages).map{ |n| JSON.parse(self.class.get("/message_threads", body: { page: n }, headers: { "authorization" => @auth_token }).body) }
+    else
+      @messages = JSON.parse(self.class.get("/message_threads", body: { page: page }, headers: { "authorization" => @auth_token }).body)
+    end
+  end
+
+  def create_message(sender_email, recipient_id, subject, message_body)
+    response = self.class.post("/messages", headers: { "authorization" => @auth_token }, body: {'sender': sender_email,  'recipient_id': recipient_id, 'subject': subject, 'stripped-text': message_body})
+
+    puts "Message sent!" if response.success?
   end
 
 end
